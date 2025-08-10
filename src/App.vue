6676e5f5c1c314f82040a7c44981d2fd2fd47ad2
o.korotkov <korot1245@mail.ru>
2025-08-10 15:06:58 +0300

    vite+vue starter

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { UINotifications } from '@/components'
+import { useHead } from '@unhead/vue'
+useHead({
+  title: 'vite+vue starter'
+})
+</script>
+
+<style scoped>
+</style>
